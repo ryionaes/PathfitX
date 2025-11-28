1803ef75ce018f4a2a737ab0f47c5afd8264a340
justinerei <142789622+justinerei@users.noreply.github.com>
2025-11-28 23:58:19 +0800

    ud

--- app/src/main/java/com/example/pathfitx/activity_get_goals.java
+++ app/src/main/java/com/example/pathfitx/activity_get_goals.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +20,8 @@ public class activity_get_goals extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_get_goals);
 
-        Button nextBtn, backBtn;
+        Button nextBtn;
+        ImageButton backBtn;
 
         nextBtn = findViewById(R.id.nextButton);
         backBtn = findViewById(R.id.backButton);
