178720b2d900ab23e6b0042839d396879ac0efa8
justinerei <142789622+justinerei@users.noreply.github.com>
2025-11-29 14:42:45 +0800

    add backend get_user

--- app/src/main/java/com/example/pathfitx/activity_get_goals.java
+++ app/src/main/java/com/example/pathfitx/activity_get_goals.java
@@ -5,16 +5,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class activity_get_goals extends AppCompatActivity {
+
+    // Variables
+    Button nextBtn;
+    ImageButton backBtn;
+    TextView setUser;
+    CheckBox chLoseWeight, chGainWeight, chMaintainWeight, chGainMuscle, chStayActive, chImproveFlexibility, chIncreaseEndurance, chBoostEnergyLevel;
+    private List<CheckBox> allGoals = new ArrayList<>(); // A list holding all checkboxes
+    boolean isProgramitacallyChanging = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +37,28 @@ public class activity_get_goals extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_get_goals);
 
-        Button nextBtn;
-        ImageButton backBtn;
-        TextView setUser;
-        CheckBox chLoseWeight, chGainWeight, chMaintainWeight, chGainMuscle, chStayActive, chImproveFlexibility, chIncreaseEndurance, chBoostEnergyLevel;
-
+        // Initialize view
         nextBtn = findViewById(R.id.nextButton);
         backBtn = findViewById(R.id.backButton);
         setUser = findViewById(R.id.setUser);
+        chLoseWeight = findViewById(R.id.checkLoseWeight);
+        chGainWeight = findViewById(R.id.checkGainWeight);
+        chMaintainWeight = findViewById(R.id.checkMaintainWeight);
+        chGainMuscle = findViewById(R.id.checkGainMuscle);
+        chStayActive = findViewById(R.id.checkStayActive);
+        chImproveFlexibility = findViewById(R.id.checkImproveFlexibility);
+        chIncreaseEndurance = findViewById(R.id.checkIncreaseEndurance);
+        chBoostEnergyLevel = findViewById(R.id.checkBoostEnergyLevels);
+
+        // Adding them to list
+        allGoals.add(chLoseWeight);
+        allGoals.add(chMaintainWeight);
+        allGoals.add(chGainWeight);
+        allGoals.add(chGainMuscle);
+        allGoals.add(chStayActive);
+        allGoals.add(chImproveFlexibility);
+        allGoals.add(chIncreaseEndurance);
+        allGoals.add(chBoostEnergyLevel);
 
         //Getting the user input from previous screen
         String name = getIntent().getStringExtra("USERNAME");
@@ -56,6 +85,53 @@ public class activity_get_goals extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        CompoundButton.OnCheckedChangeListener listener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+                if (isProgramitacallyChanging) {
+                    return;
+                }
+
+                isProgramitacallyChanging = true;
+
+                try {
+                    // LOGIC 1 > If the user checked one of the weight goals, uncheck the others.
+                    if (isChecked) {
+                        if (buttonView == chLoseWeight) {
+                            chMaintainWeight.setChecked(false);
+                            chGainWeight.setChecked(false);
+                        } else  if (buttonView == chMaintainWeight) {
+                            chLoseWeight.setChecked(false);
+                            chGainWeight.setChecked(false);
+                        } else if (buttonView == chGainWeight) {
+                            chLoseWeight.setChecked(false);
+                            chMaintainWeight.setChecked(false);
+                        }
+                    }
+
+                    int count = 0;
+                    for (CheckBox cb : allGoals) {
+                        if (cb.isChecked()) {
+                            count++;
+                        }
+                    }
+
+                    if (count > 3) {
+                        buttonView.setChecked(false);
+                        Toast.makeText(activity_get_goals.this,
+                                "You can only select up to 3 goals.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                } finally {
+                    isProgramitacallyChanging = false;
+                }
+            }
+        };
+
+        for (CheckBox cb : allGoals) {
+            cb.setOnCheckedChangeListener(listener);
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
